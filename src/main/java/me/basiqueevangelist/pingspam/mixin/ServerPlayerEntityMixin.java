2d9592e6d1b55ec9a4ea98aacd2c004dc3611e47
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-03-04 18:19:26 +0300

    Show current unread messages in actionbar

--- src/main/java/me/basiqueevangelist/pingspam/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/me/basiqueevangelist/pingspam/mixin/ServerPlayerEntityMixin.java
@@ -6,11 +6,13 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
+import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,6 +31,7 @@ public abstract class ServerPlayerEntityMixin implements ServerPlayerEntityAcces
 
     @Shadow public ServerPlayNetworkHandler networkHandler;
     @Unique private final List<Text> pings = new ArrayList<>();
+    @Unique private int actionbarTime = 0;
 
     @Override
     public List<Text> pingspam$getPings() {
@@ -44,6 +47,22 @@ public abstract class ServerPlayerEntityMixin implements ServerPlayerEntityAcces
 
         Text pingMessage = access.pingspam$getMessage().shallowCopy().formatted(Formatting.BLUE);
         networkHandler.sendPacket(new GameMessageS2CPacket(pingMessage, access.pingspam$getLocation(), access.pingspam$getSenderUuid()));
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void tick(CallbackInfo ci) {
+        if (pings.size() > 0) {
+            actionbarTime++;
+            if (actionbarTime >= 5) {
+                actionbarTime = 0;
+                networkHandler.sendPacket(new TitleS2CPacket(
+                        TitleS2CPacket.Action.ACTIONBAR,
+                        new LiteralText("You have " + pings.size() + " unread message" + (pings.size() != 1 ? "s" : "") + ".")
+                ));
+            }
+        } else {
+            actionbarTime = 5;
+        }
     }
 
     @Inject(method = "readCustomDataFromTag", at = @At("TAIL"))
