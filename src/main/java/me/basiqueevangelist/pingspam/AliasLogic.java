abc51b4053996d67a76a8c01c7c7e32360ca41f3
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-03-20 19:58:39 +0300

    Move possible name getting logic to AliasLogic

--- src/main/java/me/basiqueevangelist/pingspam/AliasLogic.java
+++ src/main/java/me/basiqueevangelist/pingspam/AliasLogic.java
@@ -6,6 +6,8 @@ import net.minecraft.nbt.Tag;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -35,5 +37,38 @@ public final class AliasLogic {
         }
 
         return false;
+    }
+
+    public static List<String> listValidNames(PlayerManager manager) {
+        List<String> possibleNames = new ArrayList<>();
+        for (ServerPlayerEntity otherPlayer : manager.getPlayerList()) {
+            String playerName = otherPlayer.getGameProfile().getName();
+            if (!possibleNames.contains(playerName))
+                possibleNames.add(playerName);
+
+            for (String alias : PlayerUtils.getAliasesOf(otherPlayer)) {
+                if (!possibleNames.contains(alias))
+                    possibleNames.add(alias);
+            }
+        }
+        for (Map.Entry<UUID, CompoundTag> offlinePlayerTag : OfflinePlayerCache.INSTANCE.getPlayers().entrySet()) {
+            if (manager.getPlayer(offlinePlayerTag.getKey()) != null)
+                continue;
+
+            if (offlinePlayerTag.getValue().contains("SavedUsername")) {
+                String offlineUsername = offlinePlayerTag.getValue().getString("SavedUsername");
+                if (!possibleNames.contains(offlineUsername))
+                    possibleNames.add(offlineUsername);
+            }
+            if (offlinePlayerTag.getValue().contains("Shortnames")) {
+                ListTag aliasesTag = offlinePlayerTag.getValue().getList("Shortnames", 8);
+                for (Tag aliasTag : aliasesTag) {
+                    String alias = aliasTag.asString();
+                    if (!possibleNames.contains(alias))
+                        possibleNames.add(alias);
+                }
+            }
+        }
+        return possibleNames;
     }
 }
--- src/main/java/me/basiqueevangelist/pingspam/network/ServerNetworkLogic.java
+++ src/main/java/me/basiqueevangelist/pingspam/network/ServerNetworkLogic.java
@@ -1,22 +1,15 @@
 package me.basiqueevangelist.pingspam.network;
 
-import me.basiqueevangelist.pingspam.OfflinePlayerCache;
-import me.basiqueevangelist.pingspam.PlayerUtils;
+import me.basiqueevangelist.pingspam.AliasLogic;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 public final class ServerNetworkLogic {
     private ServerNetworkLogic() {
@@ -32,35 +25,7 @@ public final class ServerNetworkLogic {
         newBuf.writeBoolean(Permissions.check(player, "pingspam.ping.offline", 2));
         newBuf.writeBoolean(Permissions.check(player, "pingspam.ping.player", true));
 
-        List<String> possibleNames = new ArrayList<>();
-        for (ServerPlayerEntity otherPlayer : manager.getPlayerList()) {
-            String playerName = otherPlayer.getGameProfile().getName();
-            if (!possibleNames.contains(playerName))
-                possibleNames.add(playerName);
-
-            for (String alias : PlayerUtils.getAliasesOf(otherPlayer)) {
-                if (!possibleNames.contains(alias))
-                    possibleNames.add(alias);
-            }
-        }
-        for (Map.Entry<UUID, CompoundTag> offlinePlayerTag : OfflinePlayerCache.INSTANCE.getPlayers().entrySet()) {
-            if (manager.getPlayer(offlinePlayerTag.getKey()) != null)
-                continue;
-
-            if (offlinePlayerTag.getValue().contains("SavedUsername")) {
-                String offlineUsername = offlinePlayerTag.getValue().getString("SavedUsername");
-                if (!possibleNames.contains(offlineUsername))
-                    possibleNames.add(offlineUsername);
-            }
-            if (offlinePlayerTag.getValue().contains("Shortnames")) {
-                ListTag aliasesTag = offlinePlayerTag.getValue().getList("Shortnames", 8);
-                for (Tag aliasTag : aliasesTag) {
-                    String alias = aliasTag.asString();
-                    if (!possibleNames.contains(alias))
-                        possibleNames.add(alias);
-                }
-            }
-        }
+        List<String> possibleNames = AliasLogic.listValidNames(manager);
         newBuf.writeVarInt(possibleNames.size());
         for (String possibleName : possibleNames) {
             newBuf.writeString(possibleName);
