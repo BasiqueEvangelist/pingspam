4ce39610784ac7d7291a2baf01be4ea42b4c2ae7
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-03-23 18:25:13 +0300

    Add /pingspam reload command

--- src/main/java/me/basiqueevangelist/pingspam/commands/PingSpamCommands.java
+++ src/main/java/me/basiqueevangelist/pingspam/commands/PingSpamCommands.java
@@ -1,7 +1,10 @@
 package me.basiqueevangelist.pingspam.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import me.basiqueevangelist.pingspam.PingSpam;
 import net.minecraft.server.command.ServerCommandSource;
+
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class PingSpamCommands {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
@@ -10,5 +13,14 @@ public class PingSpamCommands {
         PingSoundCommand.register(dispatcher);
         PingIgnoreCommand.register(dispatcher);
         GroupCommand.register(dispatcher);
+
+        dispatcher.register(
+            literal("pingspam")
+                .then(literal("reload")
+                    .executes(ctx -> {
+                        PingSpam.CONFIG.load();
+
+                        return 0;
+                    })));
     }
 }
