2ae4ca842dd2d602a262c7a607443c750560619d
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-04-03 22:22:08 +0300

    quickfix: only allows operators to reload config

--- src/main/java/me/basiqueevangelist/pingspam/commands/PingSpamCommands.java
+++ src/main/java/me/basiqueevangelist/pingspam/commands/PingSpamCommands.java
@@ -2,6 +2,7 @@ package me.basiqueevangelist.pingspam.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import me.basiqueevangelist.pingspam.PingSpam;
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.server.command.ServerCommandSource;
 
 import static net.minecraft.server.command.CommandManager.literal;
@@ -17,6 +18,7 @@ public class PingSpamCommands {
         dispatcher.register(
             literal("pingspam")
                 .then(literal("reload")
+                    .requires(x -> Permissions.check(x, "pingspam.reload", 2))
                     .executes(ctx -> {
                         PingSpam.CONFIG.load();
 
